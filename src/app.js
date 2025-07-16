5f79430438f88aeec5330fe475993eb352188339
ashiyaman <ashiya1005@gmail.com>
2025-07-16 12:33:58 +0530

    feat: GET and POST API

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ const User = require("./models/users")
 app.use(express.json()) //  its a middleware. We get response as JSON object. To read we need to parse through JSON. 
                         //  It is provided by express itself
                         // similar to app.use("/") => when we dont give any endpoint, its applied to all routes
-                        
 
 app.post("/signup", async(req, res) => {
 
@@ -23,14 +22,55 @@ app.post("/signup", async(req, res) => {
     //     age: 32,
     //     gender: "Female"
     // })
+    const user = new User(req.body)
+    try{
+        await user.save()
+        res.send("User saved successfully")
+    }
+    catch(error){
+        res.send(400).send("Error saving the user: ", error.message)
+    }
+})
 
-    // try{
-    //     await user.save()
-    //     res.send("User saved successfully")
-    // }
-    // catch(error){
-    //     res.send(400).send("Error saving the user: ", error.message)
-    // }
+app.get("/user", async(req, res) => {
+    try{
+        const user = await User.find({email: req.body.email})
+        if(!user){
+           res.status(404).send("User not found")
+        }
+        res.send(user)
+    }
+    catch(error){
+        res.status(400).json("Some error", error)
+    }
+})
+
+//  Get all users
+app.get("/feed", async(req, res) => {
+    try{
+        const users= await User.find()
+        if(!users){
+            res.status(404).send("No users found")
+        }
+        res.send(users)
+    }
+    catch(error){
+        res.status(400).send("Some error")
+    }
+})
+
+//  Get user by id
+app.get("/user/:userId", async(req, res) => {
+    try{
+        const user = await User.findById(req.params.userId)
+        if(!user){
+            res.status(404).send("user not found.")
+        }
+        res.send(user)
+    }
+    catch(error){
+        res.status(400).send("Something went wrong")
+    }
 })
 
 connectDB()
